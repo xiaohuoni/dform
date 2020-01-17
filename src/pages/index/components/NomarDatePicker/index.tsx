88520ec3fc7dc57f629c46e8067826a6fd6d806c
chenshuhang <437132124@qq.com>
2020-01-17 10:31:23 +0800

    fix: when mode is 'month', page show error time mode 'YYYY-MM-DD'

--- src/pages/index/components/NomarDatePicker/index.tsx
+++ src/pages/index/components/NomarDatePicker/index.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { DatePickerPropsType } from 'antd-mobile/es/date-picker/PropsType';
 import { Field } from 'rc-field-form';
 import { DatePicker, List } from 'antd-mobile';
+import moment from 'moment';
 
 import styles from '../../styles/index.less';
 
@@ -15,17 +16,43 @@ export interface INomarDatePickerProps extends DatePickerPropsType {
 }
 
 const NomarDatePicker: FC<INomarDatePickerProps> = props => {
-  const {
-    fieldProps,
-    required = false,
-    title,
-    rules,
-    modeType = 'date',
-    ...otherProps
-  } = props;
+  const { fieldProps, required = false, title, rules, modeType = 'date', ...otherProps } = props;
+
+  /**
+   * 时间展示类型改变事件
+   * @param val
+   */
+  const changeDateMode = (val: Date) => {
+    let newValue = '';
+    switch (modeType) {
+      case 'datetime':
+        newValue = moment(val).format('YYYY-MM-DD hh:mm');
+        break;
+      case 'month':
+        newValue = moment(val).format('YYYY-MM');
+        break;
+      case 'time':
+        newValue = moment(val).format('hh:mm');
+        break;
+      case 'year':
+        newValue = moment(val).format('YYYY');
+        break;
+      default:
+        newValue = moment(val).format('YYYY-MM-DD');
+        break;
+    }
+    return newValue;
+  };
+
   return (
     <Field name={fieldProps} rules={rules || [{ required, message: `请选择${title}` }]}>
-      <DatePicker {...otherProps} mode={modeType} >
+      <DatePicker 
+        {...otherProps} 
+        mode={modeType}
+        format={value => {
+          return changeDateMode(value)
+        }}
+      >
         <List.Item arrow="horizontal">
           {required && <span className={styles.redStar}>*</span>}
           {title}
